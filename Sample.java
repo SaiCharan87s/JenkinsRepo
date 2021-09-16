6455708d5ef42d984e05a4f9f80782e5dfc4f8df
SaiCharan87s <90744386+SaiCharan87s@users.noreply.github.com>
2021-09-16 11:47:23 +0530

    Create Sample.java
    Create a sample.java

---
+++ Sample.java
@@ -0,0 +1,8 @@
+class Sample
+            {
+                public static void main (String args[])
+                  
+                  {
+                        System.out.println("Hey this is just a sample code");
+                  }
+            }
